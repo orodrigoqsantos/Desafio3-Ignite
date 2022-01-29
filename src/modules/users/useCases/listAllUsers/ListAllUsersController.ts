2daf61694681cfb465552b0842669cf5cd06742a
Rodrigo Santos <didigorr@gmail.com>
2022-01-29 12:08:12 -0300

    usecase listAllUsers tests passing

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -1,12 +1,14 @@
 import { Request, Response } from "express";
+import { User } from "modules/users/model/User";
 
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
 class ListAllUsersController {
-  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+  constructor(private listAllUsersUseCase: ListAllUsersUseCase) { }
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const users: User[] = this.listAllUsersUseCase.execute(request.body);
+    return response.json(users);
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -6,10 +6,14 @@ interface IRequest {
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(private usersRepository: IUsersRepository) { }
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const userRequestor = this.usersRepository.findById(user_id);
+    if (userRequestor.admin === false) {
+      throw new Error("User does not have permission to access users list");
+    }
+    return this.usersRepository.list();
   }
 }
 
